826fed2881cb00343e28cb74fba6aabce3e0e6ff
Sam Wirch <samwirch@gmail.com>
2012-07-08 15:01:55 -0700

    added 'update settings' option

--- ao.py
+++ ao.py
@@ -8,30 +8,47 @@ def UI():
 		cmds.deleteUI("ao")
 
 	# create window
-	window = cmds.window("ao", title="Add Ambient Occlusion Layer", w=300, h=300, mxb=False, mnb=False, sizeable=False)
+	window = cmds.window("ao", title="Add Ambient Occlusion", w=300, h=300, mxb=False, mnb=False, sizeable=False)
 	
 	# create layout
 	mainLayout = cmds.columnLayout(w=300, h=300)
 	
+	# if AO exists, get attributes from shader, otherwise provide defaults
+	numSamples = 128
+	spread = 0.8
+	maxDistance = 0
+	if (aoExists()):
+		numSamples = cmds.getAttr('amb_occl.samples')
+		spread = cmds.getAttr('amb_occl.spread')
+		maxDistance = cmds.getAttr('amb_occl.max_distance')
+		
+	# create input fields
 	cmds.separator(h=15)
 	cmds.text(label="Number of samples")
-	samplesField = cmds.intField("numSamples", v=128, min=0, max=512)
+	samplesField = cmds.intField("numSamples", v=numSamples, min=0, max=512)
+	cmds.text(label="Spread")
+	spreadField = cmds.floatField("spread", v=spread)
+	cmds.text(label="Max Distance")
+	maxDistanceField = cmds.intField("maxDistance", v=maxDistance)
 	
+	# create buttons
 	cmds.separator(h=15)
 	cmds.button(label="Add Ambient Occlusion", w=300, h=30, command=addAmbOcc)
+	cmds.button(label="Update Objects/Settings", w=300, h=30, command=addAmbOcc)
 	cmds.separator(h=15)
 	
 	cmds.showWindow(window)
 	
-def createRL(numSamples):
+# creates a new ambient occlusion render layer
+def createRL(numSamples, spread, maxDistance):
 	# create a new render layer that includes all objects and all new objects
 	cmds.createRenderLayer(name='ao', g=True, makeCurrent=True)
 	# create a new surface shader for the ambient occlusion shader
 	surfShader = cmds.shadingNode('surfaceShader', asShader=True, name='amb_occl_surf_shader')
 	aoShader = cmds.shadingNode('mib_amb_occlusion', asShader=True, name='amb_occl')
 	cmds.connectAttr(aoShader+'.outValue', surfShader+'.outColor')
-	# set the number of samples to value from UI
-	cmds.setAttr(aoShader+'.samples', numSamples)
+	# set the attributes to values from UI
+	changeAOSettings(numSamples, spread, maxDistance)
 
 	# create a new shading group for the ao shader
 	sg = cmds.sets(renderable=True, noSurfaceShader=True, empty=True, name='aoSG')
@@ -40,17 +57,46 @@ def createRL(numSamples):
 	# select all objects in the layer and attach the shader
 	cmds.select(all=True)
 	cmds.hyperShade(a=surfShader)
+	
+# changes settings of mib_amb_occlusion
+def changeAOSettings(numSamples=128, spread=0.8, maxDistance=0):
+	cmds.setAttr('amb_occl.samples', numSamples)
+	cmds.setAttr('amb_occl.spread', spread)
+	cmds.setAttr('amb_occl.max_distance', maxDistance)
 
+# changes default render settings to get a decent render
 def changeRS():
 	# switch to mental ray rendering
 	cmds.setAttr('defaultRenderGlobals.ren', 'mentalRay', type='string')
 	
 def addAmbOcc(*args):
-	# get the value entered into the number of samples field
 	samplesField = cmds.intField("numSamples", q=True, v=True)
-	createRL(samplesField)
-	changeRS()
+	spreadField = cmds.floatField("spread", q=True, v=True)
+	maxDistanceField = cmds.intField("maxDistance", q=True, v=True)
 	
+	# if AO already exists, just re add all objects
+	if(aoExists()):
+		reAdd(samplesField, spreadField, maxDistanceField)
+	else:
+		# get the value entered into the number of samples field
+		createRL(samplesField, spreadField, maxDistanceField)
+		changeRS()
+
+# adds all objects to the ao layer	
+def reAdd(samplesField, spreadField, maxDistanceField):
+	# switch to the ao render layer
+	cmds.editRenderLayerGlobals( currentRenderLayer='ao' )
+	cmds.select(all=True)
+	cmds.hyperShade(a='amb_occl_surf_shader')
 	
+	changeAOSettings(samplesField, spreadField, maxDistanceField)
+	
+# returns True if ao has been generated 
+def aoExists():
+	allShaders = cmds.ls(mat=1)
+	for shader in allShaders:
+		if (shader == 'amb_occl_surf_shader'):
+			return True
+	return False
 	
 	
