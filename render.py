0f45bc88d03677cb7fe07d85dbf05b13e271fe0e
Sam Wirch <samwirch@gmail.com>
2013-11-03 22:38:55 -0800

    variable naming and documentation
    closer to pep8, and standardized variable names

--- render.py
+++ render.py
@@ -6,42 +6,50 @@ import maya.mel as mel
 from functools import partial
 
 # get render globals
-currRL = cmds.editRenderLayerGlobals( query=True, currentRenderLayer=True )
-currWidth = cmds.getAttr('defaultResolution.width')
-currHeight = cmds.getAttr('defaultResolution.height')
+curr_rl = cmds.editRenderLayerGlobals( query=True, currentRenderLayer=True )
+curr_width = cmds.getAttr('defaultResolution.width')
+curr_height = cmds.getAttr('defaultResolution.height')
 
 def UI():
-	# check to see if window exists
-	if (cmds.window('render', exists=True)):
-		cmds.deleteUI('render')
+    """generates simple button UI"""
+    # check to see if window exists
+    if (cmds.window('render', exists=True)):
+        cmds.deleteUI('render')
 
-	# create window
-	window = cmds.window('render', title='Render', w=120, h=300, mxb=False, mnb=False, sizeable=False)
-	
-	# create layout
-	mainLayout = cmds.columnLayout(w=120, h=300)
-	
-	cmds.separator(h=15)
-	cmds.columnLayout()
-	
-	cmds.button(label='Render', w=120, h=30, command=partial(render, currRL, currWidth, currHeight))
-	cmds.button(label='Render Square', w=120, h=30, command=partial(render, currRL, 1024, 1024))
-	cmds.button(label='Render AO', w=120, h=30, command=partial(render, 'ao', 1024, 1024))
-	cmds.setParent('..')
-	cmds.separator(h=15)
-	
-	# create layout
-	mainLayout = cmds.columnLayout(w=120, h=300)
-	
-	cmds.showWindow(window)
-	
+    # create window
+    window = cmds.window('render', title='Render', w=120, h=300, mxb=False, mnb=False, sizeable=False)
+    
+    # create layout
+    main_layout = cmds.columnLayout(w=120, h=300)
+    
+    cmds.separator(h=15)
+    cmds.columnLayout()
+    
+    cmds.button(label='Render', w=120, h=30, command=partial(render, curr_rl, curr_width, curr_height))
+    cmds.button(label='Render Square', w=120, h=30, command=partial(render, curr_rl, 1024, 1024))
+    cmds.button(label='Render AO', w=120, h=30, command=partial(render, 'ao', 1024, 1024))
+    cmds.setParent('..')
+    cmds.separator(h=15)
+    
+    cmds.showWindow(window)
+    
 def render(rl, width, height, *args):
-	dar = width / float(height)
-	cmds.setAttr('defaultResolution.deviceAspectRatio', dar)
-	mel.eval('RenderViewWindow')
-	cmds.Mayatomr( preview=True, layer=rl, xResolution=width, yResolution=height, camera='perspShape' )
-	editor = 'renderView'
-	cmds.renderWindowEditor(editor, e=True, si=True)
+    """
+    renders the scene with the given attributes
+
+    keyword args:
+    width -- width of render image in pixels
+    height -- height of render image in pixels
+    *args -- additional args
+
+    """
+    dar = width / float(height)
+    cmds.setAttr('defaultResolution.deviceAspectRatio', dar)
+    mel.eval('RenderViewWindow')
+    cmds.Mayatomr(preview=True, layer=rl, xResolution=width, yResolution=height, camera='perspShape')
+    editor = 'renderView'
+    cmds.renderWindowEditor(editor, e=True, si=True)
 
 def main():
+    """calls the UI function to generate the UI"""
     UI()
